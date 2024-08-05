da2cbc798c2d7bcd3ab5c35a9fcf1bb685c04a24
LSH <iks2122@gmail.com>
2024-08-05 14:36:55 +0900

    Initial project setup

---
+++ src/main/java/honeyBee/buzzyBee/BuzzyBeeApplication.java
@@ -0,0 +1,13 @@
+package honeyBee.buzzyBee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzzyBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuzzyBeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/honeyBee/buzzyBee/BuzzyBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package honeyBee.buzzyBee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuzzyBeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
